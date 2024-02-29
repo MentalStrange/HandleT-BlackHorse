5fe4e811c7aa349d5ad533510d7116f5fa66896a
mentalStrange <mohamed.ramadan2393@gmail.com>
2024-02-29 18:38:39 +0200

    make changes

--- models/regionSchema.js
+++ models/regionSchema.js
@@ -6,6 +6,16 @@ const regionSchema = new mongoose.Schema({
     required: [true, "Region should have a name"],
   },
 });
-
+regionSchema.pre('remove', async function(next){
+  try {
+    await this.model('Supplier').deleteMany({ region: this._id });
+    await this.model('Group').deleteMany({ region: this._id });
+    await this.model('Customer').deleteMany({ region: this._id });
+    await this.model('DeliveryBoy').deleteMany({ region: this._id });
+    next();
+  } catch (error) {
+    next(error);
+  }
+})
 const Region = mongoose.model("Region", regionSchema);
 export default Region
